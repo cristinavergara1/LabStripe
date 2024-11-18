c2c5a3ea90ca315da95a5a8e1de11c83939f9998
cristinavergara1 <cristina.vergara1@udea.edu.co>
2024-11-18 16:52:32 -0500

    Mod Navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,19 +1,47 @@
-"use client"
+
+"use client";
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
-import React from 'react';
 import { FiShoppingCart } from "react-icons/fi";
 
 const Navbar = () => {
+    const [cartCount, setCartCount] = useState(0);
+
+    useEffect(() => {
+        // Cargar la cantidad de artículos del carrito desde el almacenamiento local al cargar el componente
+        const cartItems = JSON.parse(localStorage.getItem("cart")) || [];
+        setCartCount(cartItems.length);
+
+        // Escuchar cambios en el carrito para actualizar el contador en el ícono del carrito
+        const updateCartCount = () => {
+            const updatedCartItems = JSON.parse(localStorage.getItem("cart")) || [];
+            setCartCount(updatedCartItems.length);
+        };
+
+        // Evento personalizado para actualizar el contador del carrito
+        window.addEventListener("cartUpdated", updateCartCount);
+
+        // Limpiar el listener cuando el componente se desmonte
+        return () => {
+            window.removeEventListener("cartUpdated", updateCartCount);
+        };
+    }, []);
+
     return (
-        <section className='shadow-xl bg-white sticky top-0'>
-            <div className='container mx-auto py-2 text-[24px] flex justify-between items-center'>
-                <div className='cursor-pointer'>
+        <section className="shadow-xl bg-white sticky top-0">
+            <div className="container mx-auto py-2 text-[24px] flex justify-between items-center">
+                <div className="cursor-pointer">
                     <Link href="/">Tienda deportiva Barbosa</Link>
                 </div>
-                <div className='relative cursor-pointer'>
+                <div className="relative cursor-pointer">
                     <Link href="/cart">
-                        <div>
+                        <div className="relative">
                             <FiShoppingCart />
+                            {cartCount > 0 && (
+                                <span className="absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 bg-red-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">
+                                    {cartCount}
+                                </span>
+                            )}
                         </div>
                     </Link>
                 </div>
