1349c9dc3c8de90952bd2ed67d2eb63e2bd104f6
cristinavergara1 <cristina.vergara1@udea.edu.co>
2024-11-18 17:01:50 -0500

    Mod cart page

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -5,32 +5,36 @@ import ButtonCheckout from '../components/ButtonCheckout';
 
 const CartPage = () => {
     const [cartItems, setCartItems] = useState([]);
+    const [totalAmount, setTotalAmount] = useState(0);
 
     useEffect(() => {
         const storedCart = JSON.parse(localStorage.getItem("cart")) || [];
         setCartItems(storedCart);
+
+        // Calcular el total al cargar el carrito
+        const total = storedCart.reduce((acc, item) => acc + (item.unit_amount || 0), 0);
+        setTotalAmount(total / 100); // Convertimos de centavos a la moneda principal
     }, []);
 
     const handleClearCart = () => {
         localStorage.removeItem("cart");
         setCartItems([]);
+        setTotalAmount(0); // Resetear el total
     };
 
     return (
         <div className="container mx-auto py-10">
-        <h1 className="text-3xl font-bold mb-5">Carrito de Compras</h1>
-    {cartItems.length === 0 ? (
-        <div>
-            <p>Tu carrito está vacío.</p>
-            <button 
-                onClick={() => window.location.href = '/pricing'} 
-                className="bg-blue-500 text-white px-4 py-2 rounded mt-4"
-            >
-                Ir a la tienda
-            </button>
-        </div>
-
-            
+            <h1 className="text-3xl font-bold mb-5">Carrito de Compras</h1>
+            {cartItems.length === 0 ? (
+                <div>
+                    <p>Tu carrito está vacío.</p>
+                    <button 
+                        onClick={() => window.location.href = '/pricing'} 
+                        className="bg-blue-500 text-white px-4 py-2 rounded mt-4"
+                    >
+                        Ir a la tienda
+                    </button>
+                </div>
             ) : (
                 <div>
                     {cartItems.map((item, index) => (
@@ -44,8 +48,14 @@ const CartPage = () => {
                             )}
                         </div>
                     ))}
+                    
+                    {/* Mostrar el total del carrito */}
+                    <div className="text-xl font-semibold mt-5">
+                        Total: {totalAmount.toFixed(2)} USD
+                    </div>
+
                     <div className="mt-5">
-                    <ButtonCheckout cartItems={cartItems} />
+                        <ButtonCheckout cartItems={cartItems} />
                         <button 
                             onClick={handleClearCart}
                             className="bg-red-600 text-white py-2 px-4 rounded ml-4"
