eee24e9ed018c4d35dd1525ae54a47a0f753d0d7
cristinavergara1 <cristina.vergara1@udea.edu.co>
2024-11-18 13:57:26 -0500

    Mod checkout route

--- src/app/api/checkout/route.js
+++ src/app/api/checkout/route.js
@@ -2,21 +2,35 @@ import { NextResponse } from "next/server";
 import { Stripe } from 'stripe';
 
 export async function POST(request) {
-  const { priceId } = await request.json();
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-  
-  const session = await stripe.checkout.sessions.create({
-    mode: 'payment', // Cambiado de 'subscription' a 'payment' para un pago único
-    payment_method_types: ['card'],
-    line_items: [
-      {
-        price: priceId,
-        quantity: 1
-      }
-    ],
-    success_url: 'http://localhost:3000/success',
-    cancel_url: 'http://localhost:3000/pricing'
-  });
-  
-  return NextResponse.json({ url: session.url });
+    const { cartItems } = await request.json(); 
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+    try {
+       
+        const lineItems = cartItems.map(item => ({
+            price_data: {
+                currency: item.currency || 'usd', 
+                product_data: {
+                    name: item.productName,
+                    images: [item.productImage], 
+                },
+                unit_amount: item.unit_amount, 
+            },
+            quantity: item.quantity || 1, 
+        }));
+
+      
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ['card'],
+            line_items: lineItems,
+            mode: 'payment',
+            success_url: 'http://localhost:3000/success',
+            cancel_url: 'http://localhost:3000/cart', 
+        });
+
+        return NextResponse.json({ url: session.url });
+    } catch (error) {
+        console.error("Error al crear la sesión de pago:", error);
+        return NextResponse.json({ error: "Error al crear la sesión de pago" }, { status: 500 });
+    }
 }
